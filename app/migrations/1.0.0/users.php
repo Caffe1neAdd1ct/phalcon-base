15cfd69737d621f880423e618f26850a02974e58
Kevin Andrews <kevin@zvps.uk>
2016-05-12 11:11:08 +0100

    Fixed users migration truncating bcrypt passwords.

--- app/migrations/1.0.0/users.php
+++ app/migrations/1.0.0/users.php
@@ -5,93 +5,128 @@ use Phalcon\Db\Index;
 use Phalcon\Db\Reference;
 use Phalcon\Mvc\Model\Migration;
 
+/**
+ * Class UsersMigration_100
+ */
 class UsersMigration_100 extends Migration
 {
-
-    public function up()
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
     {
-        $this->morphTable(
-            'users',
-            array(
-            'columns' => array(
-                new Column(
-                    'id',
-                    array(
-                        'type' => Column::TYPE_INTEGER,
-                        'notNull' => true,
-                        'autoIncrement' => true,
-                        'size' => 1,
-                        'first' => true
+        $this->morphTable('users', array(
+                'columns' => array(
+                    new Column(
+                        'id',
+                        array(
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        )
+                    ),
+                    new Column(
+                        'username',
+                        array(
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'id'
+                        )
+                    ),
+                    new Column(
+                        'password',
+                        array(
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'username'
+                        )
+                    ),
+                    new Column(
+                        'email',
+                        array(
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'password'
+                        )
+                    ),
+                    new Column(
+                        'valid',
+                        array(
+                            'type' => Column::TYPE_VARCHAR,
+                            'default' => "0",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'email'
+                        )
+                    ),
+                    new Column(
+                        'token',
+                        array(
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'valid'
+                        )
+                    ),
+                    new Column(
+                        'created',
+                        array(
+                            'type' => Column::TYPE_DATETIME,
+                            'default' => "0000-00-00 00:00:00",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'token'
+                        )
+                    ),
+                    new Column(
+                        'deleted',
+                        array(
+                            'type' => Column::TYPE_DATETIME,
+                            'size' => 1,
+                            'after' => 'created'
+                        )
                     )
                 ),
-                new Column(
-                    'username',
-                    array(
-                        'type' => Column::TYPE_INTEGER,
-                        'notNull' => true,
-                        'size' => 1,
-                        'after' => 'id'
-                    )
+                'indexes' => array(
+                    new Index('PRIMARY', array('id'), 'PRIMARY'),
+                    new Index('id_UNIQUE', array('id'), 'UNIQUE'),
+                    new Index('username_UNIQUE', array('username'), 'UNIQUE')
                 ),
-                new Column(
-                    'password',
-                    array(
-                        'type' => Column::TYPE_VARCHAR,
-                        'notNull' => true,
-                        'size' => 1,
-                        'after' => 'username'
-                    )
+                'options' => array(
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '1',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
                 ),
-                new Column(
-                    'email',
-                    array(
-                        'type' => Column::TYPE_VARCHAR,
-                        'notNull' => true,
-                        'size' => 1,
-                        'after' => 'password'
-                    )
-                ),
-                new Column(
-                    'token',
-                    array(
-                        'type' => Column::TYPE_VARCHAR,
-                        'notNull' => true,
-                        'size' => 1,
-                        'after' => 'email'
-                    )
-                ),
-                new Column(
-                    'valid',
-                    array(
-                        'type' => Column::TYPE_INTEGER,
-                        'notNull' => true,
-                        'size' => 1,
-                        'after' => 'token'
-                    )
-                ),
-                new Column(
-                    'created',
-                    array(
-                        'type' => Column::TYPE_VARCHAR,
-                        'notNull' => true,
-                        'size' => 1,
-                        'after' => 'valid'
-                    )
-                ),
-                new Column(
-                    'deleted',
-                    array(
-                        'type' => Column::TYPE_VARCHAR,
-                        'size' => 1,
-                        'after' => 'created'
-                    )
-                )
-            ),
-            'indexes' => array(
-                new Index('sqlite_autoindex_users_3', array('email')),
-                new Index('sqlite_autoindex_users_2', array('username')),
-                new Index('sqlite_autoindex_users_1', array('id'))
-            ),        )
+            )
         );
     }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
 }
